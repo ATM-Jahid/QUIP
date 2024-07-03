c6ccc5e04f18fc69f7674b7fbf3b8a863632a8d2
ATM-Jahid <ATM-Jahid@users.noreply.github.com>
2024-07-03 15:38:05 +0000

    deploy: f42b65dd82ffdb49c321cd7fa91322c5d2fdcdc1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'ee3f4a8',
+    VERSION: 'f42b65d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
